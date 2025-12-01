e1761611f00fdc44c81be6a90a309e7f91a67432
denisfelst <denis-fg@hotmail.com>
2025-12-01 15:11:23 +0100

    [TASK] Add environment settings for app launch

--- Finament.Api/Program.cs
+++ Finament.Api/Program.cs
@@ -1,29 +1,40 @@
 ﻿using Finament.Api.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Database");
 
 builder.Services.AddControllers();
+
+// --- SWAGGER ---
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo {  Title = "Finament.Api", Version = "v1", Description = "The Finament API" });
+});
 
 builder.Services.AddDbContext<FinamentDbContext>(options =>
 {
     options.UseNpgsql(connectionString);
 });
+var aaa =  Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ;
 
 var app = builder.Build();
 
 AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Finament.Api v1");
+    });
+}
 
 app.MapControllers();
 
