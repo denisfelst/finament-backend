448762fb1658e483afcfee29f8387ea71bb93d62
denisfelst <denis-fg@hotmail.com>
2026-01-04 13:45:13 +0100

    [FIX] create User DTO make fields required

--- Finament.Application/DTOs/Users/Requests/CreateUserDto.cs
+++ Finament.Application/DTOs/Users/Requests/CreateUserDto.cs
@@ -2,7 +2,7 @@ namespace Finament.Application.DTOs.Users.Requests;
 
 public class CreateUserDto
 {
-    public string Name { get; set; } = "";
-    public string Email { get; set; } = "";
+    public required string Name { get; set; }
+    public required string Email { get; set; }
     // public string Password { get; set; } = "";
 }
